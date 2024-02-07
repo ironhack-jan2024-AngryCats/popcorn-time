dc2df2479027ce7b3cb712c5d4fb58f5222a4ed5
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2024-02-07 11:58:15 +0100

    setup basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,23 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import MovieList from "./components/MovieList";
 import './App.css'
+import { Route, Routes } from "react-router-dom";
 
 function App() {
+
   return (
     <>
+
       <Header />
-      <MovieList />
+
+      <Routes>
+        <Route path="/" element={<MovieList />} />
+        <Route path="/about" element={<p>This is the About page</p>} />
+        <Route path="*" element={<p>404, this page doesnt exist...</p>} />
+      </Routes>
+
       <Footer />
+
     </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,17 @@
+import { NavLink } from "react-router-dom";
 import "./Header.css";
 
-function Header(){
-    return(
+function Header() {
+
+    return (
         <header className="Header">
             <h1>Welcome to Popcorn Time!</h1>
+
+            <nav>
+                <NavLink to="/">Home</NavLink> |
+                <NavLink to="/about">About</NavLink> |
+            </nav>
+
         </header>
     );
 }
